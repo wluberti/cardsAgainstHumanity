fa6085c8bd177a588fdb5773499242500307a59a
Wouter Luberti <wouter@zenith.freedom.nl>
2020-03-22 14:29:55 +0100

    Added docblocks

--- cah.py
+++ cah.py
@@ -14,9 +14,11 @@ NUMBER_OF_ANSWERS = 8
 
 class CardsAgainstHumanity(object):
     def __init__(self):
+        """Print a nice welcome message"""
         print("Welcome to Card Against Humanity")
 
     def getLanguages(self):
+        """Looks in the DATA_DIRECTORY to look for available languages"""
         try:
             dirs = [directory for directory in listdir(DATA_DIRECTORY) if directory.isupper() and isdir(join(DATA_DIRECTORY, directory))]
             self.availableLanguages = dirs
@@ -25,6 +27,7 @@ class CardsAgainstHumanity(object):
             exit("Could not read from 'data' directory!")
     
     def setLanguage(self, language):
+        """Sets the language used for the questions and answers"""
         if language == '':
             language = 'EN'
         if language.upper() in self.availableLanguages:
@@ -33,6 +36,7 @@ class CardsAgainstHumanity(object):
             exit('This language is not available')
 
     def loadFiles(self):
+        """Loads the questions and answers files for the set language"""
         try:
             with open (join(DATA_DIRECTORY + self.language, "questions.txt")) as questionfile:
                 self.questions = questionfile.readlines()
@@ -42,11 +46,14 @@ class CardsAgainstHumanity(object):
             exit("Could not read the question and answer files for this language:")
 
     def getQuestion(self):
+        """Get a ramdom question"""
         question = choice(self.questions)
         self.numberOfAnswersNeeded = question.count('_')
         return question.replace('_', '<.....>')
 
     def getAnswers(self, amount = NUMBER_OF_ANSWERS):
+        """Gets multile answers (as defined by NUMBER_OF_ANSWERS. Note: If a question holds
+           multiple answers the amount of answers will multiply accordingly."""
         if self.numberOfAnswersNeeded > 1:
             amount *= self.numberOfAnswersNeeded
         return sample(self.answers, amount)
